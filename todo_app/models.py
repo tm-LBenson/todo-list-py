293207884034d326ca9087287237dc1e9f909cc2
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-11 13:34:56 -0500

    add basic path managment

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+    pass
 
-# Create your models here.
+class Todo(models.Model):
+    description = models.charField(max_length=200)
+    is_completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.description
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from todo_app.models import Todo
 
-# Create your views here.
+
+
+@login_required
+def index(request):
+    context = {'todos': Todo.objects.filter(user=request.user)}
+    return render(request, 'index.html', context)
+
