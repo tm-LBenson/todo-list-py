9ba38b7a70c7822246898faa724cd10e5585f1db
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-11 12:58:16 -0500

    remove old project to start fresh

--- todo_project/todo_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import TodoItem
-
-class TodoItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TodoItem
-        fields = '__all__'
