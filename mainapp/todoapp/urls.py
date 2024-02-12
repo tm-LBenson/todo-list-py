13a7b87c502a62fb11674bfaa4222ce57d17b745
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-12 17:35:51 -0500

    setup edit modal

--- mainapp/todoapp/urls.py
+++ mainapp/todoapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TodoView, TodoDetailView, home
+from .views import TodoView, TodoDetailView, home, edit_todo_form
 
 urlpatterns = [
     path('todos/', TodoView.as_view(), name='todos'),
-    path('todos/<int:id>/', TodoDetailView.as_view(), name='todo_detail'),
+    path('todos/<int:id>/', TodoDetailView.as_view(),
+    name='todo_detail'),
+    path('todos/<int:id>/edit', edit_todo_form, name='edit_todo_form'),
     path('', home, name='home'),
 ]
--- mainapp/todoapp/views.py
+++ mainapp/todoapp/views.py
@@ -5,20 +5,21 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Todo
 from .serializers import TodoSerializer
+from django.shortcuts import get_object_or_404
 
 def home(request):
     return render(request, 'index.html')
 
 def generate_todo_html(todo):
-    # Including buttons for update and delete actions
     return f"""
     <div id="todo-{todo.id}">
         <strong>{todo.title}</strong> - <em class="tag">{todo.status}</em>
         <p>{todo.description}</p>
-        <button hx-get="/todos/{todo.id}/edit" hx-target="#todo-{todo.id}" hx-swap="outerHTML">Edit</button>
+        <button hx-get="/todos/{todo.id}/edit" hx-target="#editModal" hx-swap="outerHTML">Edit</button>
         <button hx-delete="/todos/{todo.id}/" hx-target="#todo-{todo.id}" hx-swap="outerHTML" hx-confirm="Are you sure you want to delete this todo?">Delete</button>
     </div>
     """
+
 
 class TodoView(APIView):
     def get(self, request, format=None):
@@ -54,3 +55,19 @@ class TodoDetailView(APIView):
         todo = self.get_object(id)
         todo.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+    
+def edit_todo_form(request, id):
+    todo = get_object_or_404(Todo, pk=id)
+
+    form_html = f"""
+    <div id="modalOverlay"></div>
+    <div id="editModal">
+        <form hx-post="/todos/{todo.id}/update" hx-target="#todo-{todo.id}" hx-swap="outerHTML">
+            <input type="text" name="title" value="{todo.title}" required />
+            <textarea name="description">{todo.description}</textarea>
+            <button type="submit">Save Changes</button>
+        <button hx-get="/todos/{todo.id}/cancel_edit" hx-target="#editModal">Close</button>
+        </form>
+    </div>
+    """
+    return HttpResponse(form_html)
